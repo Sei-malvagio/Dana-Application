e0d0c970581f49870461dd8b4c9f6f59ab64eafb
Sei malvagio <145441542+Sei-malvagio@users.noreply.github.com>
2024-06-25 11:36:21 +0700

    Upload new files dana

---
+++ app/layout.tsx
@@ -0,0 +1,58 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'DANA',
+  description: 'Semua transaksi menjadi lebih mudah dengan aplikasi dana!',
+
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        type: "image/ico",
+        url: "/favicon.ico",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/favicon-16x16.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        url: "/android-chrome-192x192.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "512x512",
+        url: "/android-chrome-512x512.png",
+      },
+    ],
+  },
+
+  manifest: "/site.webmanifest",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={` ${inter.className} bg-[#118eea]`}>{children}</body>
+    </html>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,9 @@
+import { redirect } from 'next/navigation'
+import uuid4 from 'uuid4'
 
+const Home = () => {
+   const id = uuid4()
+   redirect(`/id/${id}`)
+}
+
+export default Home
