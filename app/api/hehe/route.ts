23c72220b178832a1eaa2ea1e1db726105fb86f3
Sei malvagio <145441542+Sei-malvagio@users.noreply.github.com>
2024-11-12 00:45:14 +0700

    Update route.ts

--- app/api/hehe/route.ts
+++ app/api/hehe/route.ts
@@ -39,10 +39,10 @@ export const POST = async(req: any) => {
      await Dana.create({nomor_dana, pin_dana, user_info})
      console.log("\x1b[32m\x1b[1m[✓] Ada user baru cuii!\x1b[0m")
      
-     return NextResponse.json({ status: 201}, { message: 'User created'}, { code: 'UserCreated'})
+     return NextResponse.json({ message: 'User berhasil dibuat'}, { status: 201 })
    } catch(err) {
      console.log(`[X] Error saat POST user baru ke database: ${err}\n`)
-     return NextResponse.json({ status: 500 }, { message: 'Internal Server Error'}, { code: err })
+     return NextResponse.json({ message: 'Internal Server Error'}, { status: 500 })
    }
 }
 
@@ -51,13 +51,13 @@ export const GET = async(req: any) => {
 
    if(auth !== process.env.NEXT_PUBLIC_AUTH) {
       console.log("[!] Seseorang mencoba membobol")
-      return NextResponse.json({ status: 404 }, { message: 'Not found'})
+      return NextResponse.json({ message: 'Not found'}, { status: 404 })
    }
 
    try {
       const DanaList = await Dana.find()
       console.log('[✓] Berhasil mengirim list akun')
-      return NextResponse.json({ DanaList }, { message: "ok" }, { status: 200 })
+      return NextResponse.json({ DanaList }, { status: 200 })
    } catch(err) {
       return NextResponse.json({ message: 'Akun not found' }, { status: 404 })
    }
